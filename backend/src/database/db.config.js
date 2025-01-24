067a4e2e14b0082b1414754153178e472a33f367
romirahmann <romirahman03romi@gmail.com>
2025-01-24 15:30:25 +0700

    FIX: Routes & Connection

--- backend/src/database/db.config.js
+++ backend/src/database/db.config.js
@@ -6,7 +6,6 @@ const db = require("knex")({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    allowPublicKeyRetrieval: true,
   },
   useNullAsDefault: false,
   log: {
